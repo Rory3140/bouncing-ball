a0be41c2fff94de0fc3b8b7d64374a2420f6430b
Rory Wood <rorywood9@live.com>
2024-03-06 11:31:47 -0500

    testing github pages

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-if ("DeviceOrientationEvent" in window) {
-  const display = document.getElementById("display");
+const display = document.getElementById("display");
+display.innerText = "JS Linked";
 
-  window.addEventListener("deviceorientation", handleOrientation);
+let is_running = false;
+let demo_button = document.getElementById("start");
+demo_button.onclick = function (e) {
+  e.preventDefault();
 
-  function handleOrientation(event) {
-    const x = event.alpha;
-    const y = event.beta;
-    const z = event.gamma;
-
-    display.innerText = `X: ${x}\nY: ${y}\nZ: ${z}`;
-    console.log(x, y, z);
+  // Request permission for iOS 13+ devices
+  if (
+    DeviceMotionEvent &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+  ) {
+    DeviceMotionEvent.requestPermission();
   }
-} else {
-  alert("Device orientation not supported.");
+
+  if (is_running) {
+    window.removeEventListener("devicemotion", handleMotion);
+    demo_button.innerHTML = "Start demo";
+    is_running = false;
+  } else {
+    window.addEventListener("devicemotion", handleMotion);
+    document.getElementById("start_demo").innerHTML = "Stop demo";
+    is_running = true;
+  }
+};
+
+let ax, ay, az;
+function handleMotion(evt) {
+  // Access Accelerometer Data
+  ax = evt.accelerationIncludingGravity.x;
+  ay = evt.accelerationIncludingGravity.y;
+  az = evt.accelerationIncludingGravity.z;
+
+  display.innerText = `X: ${ax}\nY: ${ay}\nZ: ${az}`;
+  console.log(x, y, z);
 }
