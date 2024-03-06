a41c5223b4de73789402442c35e2ff366272a21a
Rory Wood <rorywood9@live.com>
2024-03-06 12:02:44 -0500

    testing moving an object arount the screen

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ function updateFieldIfNotNull(fieldName, value, precision = 10) {
 }
 
 function handleMotion(event) {
-  updateFieldIfNotNull("Accelerometer_x", event.acceleration.x);
-  updateFieldIfNotNull("Accelerometer_y", event.acceleration.y);
-  updateFieldIfNotNull("Accelerometer_z", event.acceleration.z);
-  updateFieldIfNotNull("Accelerometer_i", event.interval, 0);
+  const accelerationX = event.event.acceleration.x;
+  const accelerationY = event.event.acceleration.y;
+
+  updateFieldIfNotNull("Accelerometer_x", accelerationX);
+  updateFieldIfNotNull("Accelerometer_y", accelerationY);
+
+  const object = document.getElementById("object");
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+
+  // Map accelerometer values to screen coordinates
+  const newX = (accelerationX / 10) * (screenWidth / 2) + screenWidth / 2;
+  const newY = (accelerationY / 10) * (screenHeight / 2) + screenHeight / 2;
+
+  // Set object position
+  object.style.left = `${newX}px`;
+  object.style.top = `${newY}px`;
 }
 
 let demo_button = document.getElementById("start_demo");
@@ -23,5 +36,4 @@ demo_button.onclick = function (e) {
   }
 
   window.addEventListener("devicemotion", handleMotion);
-  document.getElementById("start_demo").innerHTML = "Stop demo";
 };
