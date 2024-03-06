4c507028f418023ef3f4fe5f58265f973402f5d7
Rory Wood <rorywood9@live.com>
2024-03-06 11:54:56 -0500

    new acc test

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const display = document.getElementById("display");
-display.innerText = "JS Linked";
+function updateFieldIfNotNull(fieldName, value, precision = 10) {
+  if (value != null)
+    document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+}
 
-let is_running = false;
-let demo_button = document.getElementById("start");
+function handleMotion(event) {
+  updateFieldIfNotNull("Accelerometer_x", event.acceleration.x);
+  updateFieldIfNotNull("Accelerometer_y", event.acceleration.y);
+  updateFieldIfNotNull("Accelerometer_z", event.acceleration.z);
+  updateFieldIfNotNull("Accelerometer_i", event.interval, 0);
+}
+
+let demo_button = document.getElementById("start_demo");
 demo_button.onclick = function (e) {
   e.preventDefault();
 
@@ -14,24 +22,6 @@ demo_button.onclick = function (e) {
     DeviceMotionEvent.requestPermission();
   }
 
-  if (is_running) {
-    window.removeEventListener("devicemotion", handleMotion);
-    demo_button.innerHTML = "Start demo";
-    is_running = false;
-  } else {
-    window.addEventListener("devicemotion", handleMotion);
-    document.getElementById("start_demo").innerHTML = "Stop demo";
-    is_running = true;
-  }
+  window.addEventListener("devicemotion", handleMotion);
+  document.getElementById("start_demo").innerHTML = "Stop demo";
 };
-
-let ax, ay, az;
-function handleMotion(evt) {
-  // Access Accelerometer Data
-  ax = evt.accelerationIncludingGravity.x;
-  ay = evt.accelerationIncludingGravity.y;
-  az = evt.accelerationIncludingGravity.z;
-
-  display.innerText = `X: ${ax}\nY: ${ay}\nZ: ${az}`;
-  console.log(x, y, z);
-}
