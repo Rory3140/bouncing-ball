f5c09fd6f0761e71a6570af0665e391f1927f40d
Rory Wood <rorywood9@live.com>
2024-03-06 23:07:46 -0500

    added simple server to send and recieve data

---
+++ server.js
@@ -0,0 +1,13 @@
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(data) {
+    // Broadcast received data to all connected clients
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
